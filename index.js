ad9a773d521ba5bf3850306d14b21ede0d0ebcd8
Ojasvi Chaudhary <43778431+ojasvichaudhary16@users.noreply.github.com>
2020-12-07 00:30:26 +0530

    Update index.js

--- index.js
+++ index.js
@@ -13,23 +13,40 @@ function store(){
         time:inputTime.value
     }
    localStorage.setItem(inputPno.value,JSON.stringify(userdetail));
-   var node = document.createElement("LI");                 // Create a <li> node
-   var textnode = document.createTextNode(localStorage.getItem(inputPno.value));         // Create a text node
-   node.appendChild(textnode);                              // Append the text to <li>
-   document.getElementById("ud1").appendChild(node);
+   document.location.reload(true);
+   
 
 }
 var count=0;
 function userdata(){
     for(var key in localStorage){
         count=count+1;
-        var node = document.createElement("LI");                            // Create a <li> node
-        var textnode = document.createTextNode(localStorage.getItem(key)); // Create a text node
-        node.appendChild(textnode);                                       // Append the text to <li>
-        document.getElementById("ud1").appendChild(node);
+   var node = document.createElement("LI");                 // Create a <li> node
+   var textnode = document.createTextNode(JSON.parse(localStorage.getItem(key)).name);         // Create a text node
+   node.appendChild(textnode);                              // Append the text to <li>
+   document.getElementById("ud1").appendChild(node);
+   var node1 = document.createElement("LI");                 // Create a <li> node
+   var textnode1 = document.createTextNode(JSON.parse(localStorage.getItem(key)).email);         // Create a text node
+   node1.appendChild(textnode1);                              // Append the text to <li>
+   document.getElementById("ud1").appendChild(node1);
+   var node2 = document.createElement("LI");                 // Create a <li> node
+   var textnode2 = document.createTextNode(JSON.parse(localStorage.getItem(key)).pno);         // Create a text node
+   node2.appendChild(textnode2);                              // Append the text to <li>
+   document.getElementById("ud1").appendChild(node2);
+   var node3 = document.createElement("LI");                 // Create a <li> node
+   var textnode3 = document.createTextNode(JSON.parse(localStorage.getItem(key)).date);         // Create a text node
+   node3.appendChild(textnode3);                              // Append the text to <li>
+   document.getElementById("ud1").appendChild(node3);
+   var node4 = document.createElement("LI");                 // Create a <li> node
+   var textnode4 = document.createTextNode(JSON.parse(localStorage.getItem(key)).time);         // Create a text node
+   node4.appendChild(textnode4);                              // Append the text to <li>
+   document.getElementById("ud1").appendChild(node4);
+   var linebreak = document.createElement('HR');
+   document.getElementById("ud1").appendChild(linebreak);
         if(count==localStorage.length){
             break;
         }
     }
-}
+  }
+
 
